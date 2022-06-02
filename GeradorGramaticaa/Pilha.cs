db4977ac14b4c52e6f38f65efcb64902d7a1396d
Alyson Matias Cechinel <106636904+alysonmcechinel@users.noreply.github.com>
2022-06-01 22:20:45 -0300

    Add files via upload

---
+++ GeradorGramaticaa/Pilha.cs
@@ -0,0 +1,75 @@
+﻿
+namespace GeradorGramaticaa
+{
+    public class Pilha
+    {
+        private string[] valores;
+        private int topo;
+
+        public Pilha(int n) /* Método construtor */
+        {
+            if (n > 0)
+            {
+                valores = new string[n];
+                topo = -1;
+            }
+        }
+
+        public int Push(string valor)   /* Método para empilhar */
+        {
+            if (topo < valores.Length - 1)
+            {
+                topo++;
+                valores[topo] = valor;
+                return 0;
+            }
+            return -1;
+        }
+
+        public string Pop() /* Método para desempilhar */
+        {
+            if (topo >= 0)
+            {
+                string valor = valores[topo];
+                topo--;
+                return valor;
+            }
+            else return null;  /* Stack Underflow */
+        }
+
+        public string ImprimirPilha()    /* Método para imprimir a pilha */
+        {
+            string saida = " ";
+            if (topo >= 0)
+            {
+                for (int i = topo; i >= 0; i--)
+                {
+                    saida = saida + valores[i] + " ";
+                }
+                return saida;
+            }
+            else return "\tPilha Vazia";
+        }
+
+        public string VerificaElementoTerminal(char b)
+        {
+            string saida = " ";
+            if (topo >= 0)
+            {
+                for (int i = topo; i >= 0; i--)
+                {
+                    if (valores[i] == valores[i].ToUpper())
+                    {
+                        valores[i] = b.ToString();
+                        saida = saida + valores[i];
+                    }
+                    else
+                        saida = saida + valores[i] + " ";
+
+                }
+                return saida;
+            }
+            else return "Erro ao gerar Gramatica";
+        }
+    }
+}
---
+++ GeradorGramaticaa/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace GeradorGramaticaa
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nInsira a Gramatica que deseja resolver\n");
+            string G = Console.ReadLine();
+
+            string[] producoesG = G.Split(new char[] { '|' });
+            int tamanhoPilha = G.Length;
+            Pilha pilha = new Pilha(tamanhoPilha);
+            int sair = 0;
+            int inserir = 0;
+            string imprime = "";
+
+            while (sair == 0)
+            {
+                if (inserir == 0)
+                {
+                    foreach (string b in producoesG)
+                    {
+                        char[] letras = b.ToCharArray();
+                        foreach (char c in letras)
+                        {
+                            pilha.Push(c.ToString());
+                        }
+                    }
+                    inserir++;
+                }
+
+
+                imprimeOpcoes();
+
+                int opcao = int.Parse(Console.ReadLine());
+                if (opcao == 0)
+                {
+                    sair = 1;
+                }
+                else
+                    if (opcao == 1)
+                    {
+                        Console.WriteLine("\nInsira a Gramatica que deseja resolver\n");
+                        string S = Console.ReadLine();
+                        string[] solucoes = S.Split(new char[] { '=' });
+
+                        foreach (char c in solucoes[1])
+                        {
+                            pilha.VerificaElementoTerminal(c);
+                        }
+                        Console.Clear();
+                    }
+                    else
+                        if (opcao == 2)
+                        {
+                            Console.Clear();
+                            imprime = pilha.ImprimirPilha();
+                            Console.WriteLine(imprime);
+                        }
+            }
+        }
+        static public void imprimeOpcoes()
+        {
+            Console.WriteLine("\nEscolha uma opção:\n");
+            Console.WriteLine("0 => Sair");
+            Console.WriteLine("1 => Sentenca");
+            Console.WriteLine("2 => Imprime gramatica");
+        }
+    }
+}
